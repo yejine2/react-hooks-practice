b0c215c68e182b3b64665f42ee8623248d9056bb
yejine2 <yejine2@naver.com>
2022-09-14 01:13:04 +0900

    feat: Use useEffect for loaded ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -8,6 +8,27 @@ const Ingredients = () => {
   // ingredients에 재료 목록이 저장되기 때문에 배열을 초기값으로 사용
   // 재료가 추가되거나 삭제될 경우에 전체가 업데이트
   const [userIngredients, setUserIngredients] = useState([]);
+
+  /* useEffect() 첫번째 인수: 렌더링이 끝난 뒤에 실행되는 함수, 
+  두번째 인수: 함수에서 사용하는 의존성(dependencies)이 저장된 배열, 이 의존성 배열이 변경될 때만 함수가 다시 실행
+  빈 배열이면 컴포넌트가 처음 렌더링될 때만 실행 그 후로는 절대 실행되지 않는다.*/
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-update-a3dd5-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
   // Firebase 연결
   const addIngredientHandler = (ingredient) => {
