b1ae06e2225242ed534dacbfbd19d68851662f87
yejine2 <yejine2@naver.com>
2022-09-13 23:22:34 +0900

    feat: Add addIngredientHandler

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
+const Ingredients = () => {
+  // ingredients에 재료 목록이 저장되기 때문에 배열을 초기값으로 사용
+  // 재료가 추가되거나 삭제될 경우에 전체가 업데이트
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  // 기존에 있던 ingredients 목록에 새로 받은 값을 추가해 업데이트 하기
+  // prevIngredients는 현재 state이다.
+  // id, title, amount를 가진 새로운 객체가 ingredients 목록에 추가
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      {
+        id: Math.random().toString(),
+        ...ingredient,
+      },
+    ]);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      {/* Add Ingredient 버튼이 있는 form에 props로 전달 */}
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
