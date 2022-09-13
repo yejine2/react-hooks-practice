a3570aa35b54755381b09b37e785558f4cdf3f6f
yejine2 <yejine2@naver.com>
2022-09-13 22:45:45 +0900

    refactor: Refactor useState use array destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -6,7 +6,7 @@ import "./IngredientForm.css";
 const IngredientForm = React.memo((props) => {
   // title과 amount의 입력값에 사용될 state
   // input의 요소값은 문자열이기 때문에 문자열로 초기화
-  const inputState = useState({ title: "", amount: "" });
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -19,15 +19,13 @@ const IngredientForm = React.memo((props) => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            {/* 2-way binding, inputState 배열에는 두 개의 요소가 있고, 0번 인덱스에 있는 첫 번째 요소는 현재 상태의 스냅샷 */}
             <input
               type="text"
               id="title"
-              value={inputState[0].title}
-              // 입력된 키는 newTitle에 값을 저장하고, 키가 입력될 때마다 새로 가져온 타이틀을 사용하게 한다.
+              value={inputState.title}
               onChange={(event) => {
                 const newTitle = event.target.value;
-                inputState[1]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   title: newTitle,
                   amount: prevInputState.amount,
                 }));
@@ -39,11 +37,11 @@ const IngredientForm = React.memo((props) => {
             <input
               type="number"
               id="amount"
-              value={inputState[0].amount}
+              value={inputState.amount}
               // 새로운 객체를 inputState에 전달해 현재 상태를 새로운 객체의 데이터로 업데이트
               onChange={(event) => {
                 const newAmount = event.target.value;
-                inputState[1]((prevInputState) => ({
+                setInputState((prevInputState) => ({
                   amount: newAmount,
                   title: prevInputState.title,
                 }));
